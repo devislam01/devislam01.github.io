ff8ebc40a1a81086c0b7b54318aefd7be944736d
Devis Lam <devislam@alphareds.com>
2025-06-20 15:50:22 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
@@ -7,7 +5,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
